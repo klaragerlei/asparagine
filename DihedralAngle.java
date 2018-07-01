ba9924baa18127746b4438afb63bdc9313b3f9f8
klaragerlei <klarizsofi@gmail.com>
2018-07-01 03:16:57 +0100

    Update DihedralAngle.java

--- DihedralAngle.java
+++ DihedralAngle.java
@@ -1,7 +1,7 @@
 
-	package Asparagine;
+package Asparagine;
 
-	import java.io.BufferedWriter;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
